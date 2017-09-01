528b6e00ae1eb1d2119165f8050cbfed2dae2412
shaowns <shaown@csebuet.org>
2017-09-01 16:26:18 -0400

    saving

--- sources.js
+++ sources.js
@@ -1,5 +1,6 @@
 const CDP = require('chrome-remote-interface');
 const chromeLauncher = require('chrome-launcher');
+const delay = require('delay');
 
 // Imported class.
 var Script = require('./Script.js');
@@ -22,13 +23,14 @@ function launchChrome(headless=true) {
 }
 
 /**
- * Process the scrapped contents from the URL.
+ * Process the scrapped materials from the URL.
  * 
- * @param {any} URL 
- * @param {any} scripts 
- * @param {any} requests 
+ * @param {String} url 
+ * @param {Dictionary} scripts 
+ * @param {Array} requests 
+ * @param {String} finalHtml
  */
-function processUrlContents(URL, scripts, requests) {
+function processUrlContents(url, scripts, requests, finalHtml) {
     // Dump the script dictionary to console.
     for (var key in scripts) {
         scripts[key].printToConsole();
@@ -38,88 +40,141 @@ function processUrlContents(URL, scripts, requests) {
     for (let r of requests) {
         console.log(r);
     }
+
+    //console.log(finalHtml);
+}
+
+/**
+ * Takes a string and scrapes off the sources, requests, and the
+ * final page content from the given url.
+ * 
+ * @param {String} url 
+ * @param {any} Network 
+ * @param {any} Debugger 
+ * @param {any} Page 
+ * @param {any} Runtime 
+ * @returns Object packed with the source scripts, requests, and the final page content.
+ */
+async function scrape(url, Network, Debugger, Page, Runtime) {
+    // Container for storing the script objects.
+    var scripts = {};
+    
+    // Container for request URLs.
+    var requests = [];
+
+    // Setup handlers for: 
+    // request about to be sent.
+    Network.requestWillBeSent((params) => {
+        requests.push(params.request.url);
+    });
+
+    // Scripts parsed.
+    Debugger.scriptParsed(async (params) => {
+        var s = new Script(params, false);
+
+        // Fetch the source of the script from the debugger.
+        // Retrieve the script source as a promise.
+        let source = await Debugger.getScriptSource({scriptId: s.getScriptId()});
+        s.setSource(source.scriptSource);
+        scripts[s.getScriptId()] = s;
+    });
+
+    // Scripts failed to parse.
+    Debugger.scriptFailedToParse(async (params) => {
+        var s = new Script(params, true);
+
+        // Fetch the source of the script from the debugger.
+        // Retrieve the script source as a promise.
+        let source = await Debugger.getScriptSource({scriptId: s.getScriptId()});
+        s.setSource(source.scriptSource);
+        scripts[s.getScriptId()] = s;
+    });
+
+    // Enable events, then start.
+    await Promise.all([Network.enable(), Page.enable(), Debugger.enable()]);
+    await Network.setCacheDisabled({cacheDisabled: true});
+    await Page.navigate({url: url});
+    await Page.loadEventFired();
+    const finalPageContent = await Runtime.evaluate({
+        expression: 'document.documentElement.outerHTML'
+    });
+
+    /* 
+    * Wait for 5000 ms before returning so that we can finish latching
+    * onto any delayed request and consequent script content.
+    * See https://github.com/shaowns/chromejsscrapper/issues/1
+    */
+    await delay(5000);
+
+    return {requests: requests, scripts: scripts, finalHtml: finalPageContent.result.value};
+}
+
+/**
+ * Initiate the scrapping of sources and process the retrieved contents.
+ * 
+ * @param {String} url 
+ * @param {any} Network 
+ * @param {any} Debugger 
+ * @param {any} Page 
+ * @param {any} Runtime 
+ */
+async function runAndProcessScrappedContents(url, Network, Debugger, Page, Runtime) {
+    // Get the scraped contents from the url given.
+    let contents = await scrape(url, Network, Debugger, Page, Runtime);
+    
+    // Process the contents we just scraped.
+    let scripts = contents.scripts;
+    let requests = contents.requests;
+    let finalHtml = contents.finalHtml;
+    
+    processUrlContents(url, scripts, requests, finalHtml);
+}
+
+/**
+ * Initializes the chrome instance in headless mode,
+ * also initializes the chrome devtools protocol client.
+ * 
+ * @returns the chrome and the protocol client instance.
+ */
+async function init() {
+    // Wait for chrome to launch
+    var chrome = await launchChrome(false);
+    
+    // Connect to endpoint
+    var client = await CDP({port: chrome.port});
+
+    return {chrome: chrome, client: client};
 }
 
 /**
  * Main function that launches an instance of chrome
  * and retrieves the information from it through the
  * devtools protocol.
- * 
- * @param {String} URL 
+ *  
  */
-var run = async function (URL) {
-    try {
-        // Wait for chrome to launch
-        var chrome = await launchChrome(false);
+var run = async function () {
+    try {        
+        var agents = await init();
 
-        // Connect to endpoint
-        var client = await CDP({port: chrome.port});
+        // Expand into the required agents.
+        var chrome = agents.chrome;
+        var client = agents.client;
 
         // Extract required domains
         const {Network, Page, Debugger, Runtime} = client;
 
-        // Container for storing the script objects.
-        var scripts = {};
-
-        // Container for request URLs.
-        var requests = [];
-
-        // Setup handlers for
-        
-        // request about to be sent.
-        Network.requestWillBeSent((params) => {
-            requests.push(params.request.url);
-        });
-
-        // Scripts parsed.
-        Debugger.scriptParsed(async (params) => {
-            var s = new Script(params, false);
-
-            // Fetch the source of the script from the debugger.
-            // Retrieve the script source as a promise.
-            let source = await Debugger.getScriptSource({scriptId: s.getScriptId()});
-            s.setSource(source.scriptSource);
-            scripts[s.getScriptId()] = s;
-        });
-
-        // Scripts failed to parse.
-        Debugger.scriptFailedToParse(async (params) => {
-            var s = new Script(params, true);
-
-            // Fetch the source of the script from the debugger.
-            // Retrieve the script source as a promise.
-            let source = await Debugger.getScriptSource({scriptId: s.getScriptId()});
-            s.setSource(source.scriptSource);
-            scripts[s.getScriptId()] = s;
-        });
-
-        // Enable events, then start.
-        await Promise.all([Network.enable(), Page.enable(), Debugger.enable()]);
-        await Network.setCacheDisabled({cacheDisabled: true});
-        await Page.navigate({url: URL});
-        await Page.loadEventFired();
-        const pageContent = await Runtime.evaluate({
-            expression: 'document.documentElement.outerHTML'
-        }); 
-        
-        /*
-        * Wait for a certain period of time before processing the scripts and the urls.
-        * See https://github.com/shaowns/chromejsscrapper/issues/1
-        */
-        setTimeout(function() {
-            processUrlContents(URL, scripts, requests);
-        }, 5000);
-
+        // TODO: Do the following for multithread or multi process way.
+        await runAndProcessScrappedContents('http://google.com', Network, Debugger, Page, Runtime);
     } catch (error) {
         console.error(error)
     } finally {
         if (client && chrome) {
-            setTimeout(async function() {
-                await client.close();
-                await chrome.kill();    
-            }, 5000);            
+            
+            await client.close();
+            await chrome.kill();
+
         }
     }
 }
 
-run('https://google.com');
+run();
