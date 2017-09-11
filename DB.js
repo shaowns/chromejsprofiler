2e8778ba38fbef1b4cabe9217a843d58140c12f8
ssarker <ssarker@ncsu.edu>
2017-09-11 18:53:12 -0400

    saving

--- DB.js
+++ DB.js
@@ -1,6 +1,32 @@
-var MongoClient = require('mongodb').MongoClient;
+// Bring Mongoose into the app
+var mongoose = require( 'mongoose' );
 
-function DB() {
-    // The MongoDB database connection.
-    this.db = null;
-}
+// Build the connection string
+var dbURI = 'mongodb://moleadmin:pieinthesky@localhost:27017/mole';
+
+// Create the database connection
+mongoose.connect(dbURI);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open to ' + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
