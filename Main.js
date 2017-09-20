1bcaa3f4db36a2630479497b33b92774b1b90c9b
shaowns <shaown@csebuet.org>
2017-09-20 15:44:25 -0400

    saving

--- Main.js
+++ Main.js
@@ -28,6 +28,9 @@ async function launchChrome(headless=true) {
 
 /**
  * Process the scrapped materials from the URL.
+ * For debugging purposes, not to be used with
+ * large scale runs, unless you want to clutter
+ * your terminal.
  * 
  * @param {ScrappedContent} content 
   */
@@ -36,134 +39,11 @@ function processUrlContents(content) {
         s.dumpToConsole();
     }
 
-    // Dump the requests to console.
     for (let r of content.requests) {
         r.dumpToConsole();
     }
 
-    //console.log(finalHtml);
-}
-
-/**
- * Takes a string and scrapes off the sources, requests, and the
- * final page content from the given url.
- * 
- * @param {String} url
- * @param {Number} rank
- * @param {any} Network 
- * @param {any} Debugger 
- * @param {any} Page 
- * @param {any} Runtime 
- * @returns Object packed with the source scripts, requests, and the final page content.
- */
-async function scrape(url, rank, Network, Debugger, Page, Runtime) {
-    // The scrapped content model object.
-    var content = new ScrappedContent();
-    content.url = url;
-    content.rank = rank;
-
-    // Container for storing the script objects.
-    var scripts = [];
-    
-    // Container for request objects.
-    var requests = [];
-
-    // Variable for start time reference point.
-    var hrstart = null;
-
-    // Setup handlers for: 
-    // request about to be sent.
-    Network.requestWillBeSent((params) => {
-        let hrend = process.hrtime(hrstart);
-        let elapsed = hrend[0] * 1000 + hrend[1]/1000000;
-
-        var request = new Request();
-        request.url = params.request.url;
-        request.timeFromPageLoad = elapsed;
-        requests.push(request);
-    });
-
-    // Scripts parsed.
-    Debugger.scriptParsed(async (params) => {
-        var script = new Script();
-        script.scriptId = params.scriptId;
-        script.url = params.url;
-        script.startLine = params.startLine;
-        script.startColumn = params.startColumn;
-        script.endLine = params.endLine;
-        script.endColumn = params.endColumn;
-        script.hash = params.hash;
-        script.failedToParse = false;
-
-        // Fetch the source of the script from the debugger.
-        // Retrieve the script source as a promise.
-        let source = await Debugger.getScriptSource({scriptId: params.scriptId});
-        script.source = source.scriptSource;
-        scripts.push(script);
-    });
-
-    // Scripts failed to parse.
-    Debugger.scriptFailedToParse(async (params) => {
-        var script = new Script();
-        script.scriptId = params.scriptId;
-        script.url = params.url;
-        script.startLine = params.startLine;
-        script.startColumn = params.startColumn;
-        script.endLine = params.endLine;
-        script.endColumn = params.endColumn;
-        script.hash = params.hash;
-        script.failedToParse = true;
-
-        // Fetch the source of the script from the debugger.
-        // Retrieve the script source as a promise.
-        let source = await Debugger.getScriptSource({scriptId: params.scriptId});
-        script.source = source.scriptSource;
-        scripts.push(script);
-    });
-
-    // Enable events, then start.
-    await Promise.all([Network.enable(), Page.enable(), Debugger.enable()]);
-    await Network.setCacheDisabled({cacheDisabled: true});
-
-    // Timing start point and navigate to the url.
-    hrstart = process.hrtime();
-    await Page.navigate({url: url});
-    await Page.loadEventFired();
-
-    // Get the final html.
-    const finalPageContent = await Runtime.evaluate({
-        expression: 'document.documentElement.outerHTML'
-    });
-
-    /* 
-    * Wait for 5000 ms before returning so that we can finish latching
-    * onto any delayed request and consequent script content.
-    * See https://github.com/shaowns/chromejsscrapper/issues/1
-    */
-    await delay(5000);
-
-    // Save the scripts, requests, and the final html into the content object.
-    content.scripts = scripts;
-    content.requests = requests;
-    content.finalHtml = finalPageContent.result.value;
-
-    return content;
-}
-
-/**
- * Initiate the scrapping of sources and process the retrieved contents.
- * 
- * @param {String} url
- * @param {Number} rank 
- * @param {any} Network 
- * @param {any} Debugger 
- * @param {any} Page 
- * @param {any} Runtime 
- */
-async function runAndProcessScrappedContents(url, rank, Network, Debugger, Page, Runtime) {
-    // Get the scraped contents from the url given.
-    let content = await scrape(url, rank, Network, Debugger, Page, Runtime);
-    processUrlContents(content);
+    console.log(finalHtml);
 }
 
 /**
@@ -211,15 +91,116 @@ async function runScrapper(startLine, endLine) {
         // Get the Alexa dataset filepath from the properties.
         var filePath = require('./property').urlDataset;
 
+        // Container for storing the script objects.
+        var scripts = null;
+        
+        // Container for request objects.
+        var requests = null;
+
+        // Variable for start time reference point.
+        var hrstart = null;
+
+        // Setup handlers for: 
+        // request about to be sent.
+        Network.requestWillBeSent((params) => {
+            let hrend = process.hrtime(hrstart);
+            let elapsed = hrend[0] * 1000 + hrend[1]/1000000;
+
+            var request = new Request();
+            request.url = params.request.url;
+            request.timeFromPageLoad = elapsed;
+            requests.push(request);
+        });
+
+        // Scripts parsed.
+        Debugger.scriptParsed(async (params) => {
+            var script = new Script();
+            script.scriptId = params.scriptId;
+            script.url = params.url;
+            script.startLine = params.startLine;
+            script.startColumn = params.startColumn;
+            script.endLine = params.endLine;
+            script.endColumn = params.endColumn;
+            script.hash = params.hash;
+            script.failedToParse = false;
+
+            // Fetch the source of the script from the debugger.
+            // Retrieve the script source as a promise.
+            let source = await Debugger.getScriptSource({scriptId: params.scriptId});
+            script.source = source.scriptSource;
+            scripts.push(script);
+        });
+
+        // Scripts failed to parse.
+        Debugger.scriptFailedToParse(async (params) => {
+            var script = new Script();
+            script.scriptId = params.scriptId;
+            script.url = params.url;
+            script.startLine = params.startLine;
+            script.startColumn = params.startColumn;
+            script.endLine = params.endLine;
+            script.endColumn = params.endColumn;
+            script.hash = params.hash;
+            script.failedToParse = true;
+
+            // Fetch the source of the script from the debugger.
+            // Retrieve the script source as a promise.
+            let source = await Debugger.getScriptSource({scriptId: params.scriptId});
+            script.source = source.scriptSource;
+            scripts.push(script);
+        });
+
+        // Enable events, then start.
+        await Promise.all([Network.enable(), Page.enable(), Debugger.enable()]);
+        await Network.setCacheDisabled({cacheDisabled: true});
+
+        // Start loading the pages one by one.
         for (var i = startLine; i <= endLine; i++) {
             var line = await nthline(i, filePath);
             var vals = line.split(",");
             var rank = parseInt(vals[0]);
             var url = vals[1];
-            console.log(rank + ' ' + url);
-            //await runAndProcessScrappedContents(url, rank, Network, Debugger, Page, Runtime);
-        }        
 
+            console.log(rank + ' ' + url);
+
+            // Clear out the script and the request container.
+            scripts = [];
+            requests = [];
+            
+            // The scrapped content model object.
+            let content = new ScrappedContent();
+            content.url = url;
+            content.rank = rank;
+
+            // Timing start point and navigate to the url.
+            hrstart = process.hrtime();
+            await Page.navigate({url: url});
+            await Page.loadEventFired();
+
+            // Get the final html.
+            const finalPageContent = await Runtime.evaluate({
+                expression: 'document.documentElement.outerHTML'
+            });
+
+            /* 
+            * Wait for 5000 ms before returning so that we can finish latching
+            * onto any delayed request and consequent script content.
+            * See https://github.com/shaowns/chromejsscrapper/issues/1
+            */
+            await delay(5000);
+
+            // Save the scripts, requests, and the final html into the content object.
+            content.scripts = scripts;
+            content.requests = requests;
+            content.finalHtml = finalPageContent.result.value;
+
+            // Save the content in the DB.
+            content.save(function(error) {
+                if (error) {
+                    throw error;
+                }
+            });
+        }
     } catch (error) {
         console.error("Error loading pages and extracting " + String(error));
     } finally {
@@ -249,9 +230,10 @@ function main() {
             var worker = cluster.fork();
             
             var sliceStart = i * sliceSize;
-            var sliceEnd = i == numWorkers - 1 ? lineLimit - 1 : sliceStart + sliceSize - 1;
+            var sliceEnd = (i == numWorkers - 1) ? lineLimit - 1 : sliceStart + sliceSize - 1;
 
             worker.send({
+                type: 'scrape',
                 startLine: sliceStart,
                 endLine: sliceEnd
             });
@@ -272,8 +254,6 @@ function main() {
             console.log('Worker ' + worker.process.pid + ' is online');
         });
 
-        
-
         cluster.on('exit', function(worker, code, signal) {
             console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
             // Check if there are no more workers, then gracefully exit the master to close mongoose connections.
@@ -282,18 +262,20 @@ function main() {
             }
         });
     } else {
-        process.on('message', async function(message) {        
-            var exitCode = await runScrapper(message.startLine, message.endLine);
-            // If there was an exit code then we would have to close this one and create a
-            // new worker with the same task as this one.
-            if (exitCode) {
-                process.send({
-                    type: 'fail',
-                    startLine: message.startLine,
-                    endLine: message.endLine
-                });
-            }
-            process.exit(0);
+        process.on('message', async function(message) {
+            if (message.type == 'scrape') {
+                var exitCode = await runScrapper(message.startLine, message.endLine);
+                // If there was an exit code then we would have to close this one and create a
+                // new worker with the same task as this one.
+                if (exitCode) {
+                    process.send({
+                        type: 'fail',
+                        startLine: message.startLine,
+                        endLine: message.endLine
+                    });
+                }
+                process.exit(exitCode);
+            }            
         });
     }
 }
